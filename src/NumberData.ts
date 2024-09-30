737c310f519c4a184a16486d2ec22b5e4e34c78f
Karin Silfversparre <ks224ac@student.lnu.se>
2024-09-30 15:56:59 +0200

    Add methods to NumberData

--- src/NumberData.ts
+++ src/NumberData.ts
@@ -8,4 +8,16 @@ export class NumberData {
     this.number2 = number2
     this.calculatedNumber = calculatedNumber
   }
+
+  addOneToNumber1 () {
+    this.number1 = this.number1 + 1
+  }
+
+  addOneToNumber2 () {
+    this.number2 = this.number2 + 1
+  }
+
+  addOneToCalculatedNumber () {
+    this.calculatedNumber = this.calculatedNumber + 1
+  }
 }
